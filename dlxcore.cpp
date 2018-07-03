3073f97820aedb3a672145767e4ff3460bc72cb7
propertime <pqf76045521@126.com>
2018-07-03 17:24:01 +0800

    insert

--- dlxcore.cpp
+++ dlxcore.cpp
@@ -1,32 +1,42 @@
-#include <iostream>
+/* 
+ * Abstract data structure for 0-1 matrix
+ * Used to solve exact cover problem
+ * Sudoku is a typical ECP
+ */
 
-typedef Node *pos;
+#include <iostream>
+#include <vector>
+
+typedef class Node *pos;
 
 class Node
 {
 public:
-    bool data;
     pos left;
     pos right;
     pos up;
     pos down;
+    int row;
+    int col;
 };
 
 class DancingLinks
 {
 public:
     pos head;
+    const int column;
 
-    DancingLinks(const int col)
+    DancingLinks(const int col = 0) : column(col)
     {
-        head = calloc(1, sizeof(struct Node));
+        head = new Node;
         head->up = head->down = head;
         pos prev = head;
         pos cur;
-        for (int i = 0; i < col; ++i)
+        for (int i = 0; i < column; ++i)
         {
-            cur = calloc(1, sizeof(struct Node));
-            cur->data = i;
+            cur = new Node;
+            cur->row = -1;
+            cur->col = i;
             cur->up = cur->down = cur;
             cur->left = prev;
             prev->right = cur;
@@ -38,11 +48,49 @@ public:
 
     ~DancingLinks()
     {
-
+        pos prev = head;
+        for (int i = 0; i < column; ++i)
+        {
+            pos cur = prev->right;
+            
+        }
     }
+
+    void Insert(const int row, const std::vector<int> &colvec);
+    pos FindCol(const int col);
 };
 
-void createdlx()
+// Linear search O(column)
+pos DancingLinks::FindCol(const int col)
 {
+    pos cur = head;
+    for (int i = 0; i <= col; ++i, cur = cur->right);
 
+    return cur;
 }
+
+void DancingLinks::Insert(const int row, const std::vector<int> &colvec)
+{
+    pos prev = nullptr, cur, first;
+    for (auto col : colvec)
+    {
+        pos colNode = FindCol(col);
+        cur = new Node;
+        cur->row = row;
+        cur->col = col;
+        cur->left = prev;
+        if (prev != nullptr)
+            prev->right = cur;
+        else
+            first = cur;
+        cur->up = colNode->up;
+        cur->down = colNode;
+        colNode->up->down = cur;
+        colNode->up = cur;
+        prev = cur;
+    }
+    cur->right = first;
+    first->left = cur;
+}
+
+void DancingLinks::Delete(const int )
